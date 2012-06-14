79d8b97354b7eea0fc9c4912181c36005370f771
Spencer Lyon <spencerlyon2@gmail.com>
2012-06-14 12:49:37 -0600

    renamed SVAR to SVAR_shortrun.py in anticipation of SVAR_longrun.py

--- Econ Homework/SVAR/SVAR_shortrun.py
+++ Econ Homework/SVAR/SVAR_shortrun.py
@@ -4,7 +4,7 @@ Created June 14, 2012
 Author: Spencer Lyon
 """
 import numpy as np
-import sympy as sym
+from sympy import symbols, nsolve
 import numpy.linalg as npla
 import scipy as sp
 import scipy.stats as st
@@ -42,54 +42,45 @@ def lag_op(y, nlag):
 
 no_lag = y[:,0:-8]
 lagged = lag_op(y, 8)
-t = lagged.shape[1]
+n_vars, t = no_lag.shape
 const_row = np.ones(t)
 
-## Do the OLS.
-# We will flip it on its side when compared to the standard.
-# THe first row will be all 1's (for the intercept term), then each row below
-# that will be the lagged data set starting from no lag going to 8 lags.
-# Note that this will be a block matrix where each 'row' as stated above is
-# actually a comparable GDP and Unem lag set.
-
+## Do the OLS. to get the B matricies and residuals.
 X = np.vstack([const_row, lagged])
 
 # This beta will be 2 x 17. The first column is the constant terms.
-# Each set of two columns after that are the coefficients for the lag n params.
 Beta = npla.lstsq(X.T, no_lag.T)[0].T
 
 # We now find the predicted y values so we can find the residuals.
 Y_hat = np.dot(X.T, Beta.T).T
 resid = no_lag - Y_hat
-
 sigma = np.cov(resid) # This is the sigma we will use to find A_0 inverse.
-print sigma
 
-# Applying short run restrictions means that A_0 inv. will have no (2,1) entry.
-# We will be left with [[a11**2, a21**2], [0, a22**2]]
+## Solving for A_0_inv
+# Applying short run restrictions means that A_0 inv. will have no (1,2) entry.
+# We will be left with sigma = [[a11**2, 0], [a11*a22, a21**2 + a22**2]]
+# We solve this equation symbolically below.
+a11, a21, a22 = symbols(['a11', 'a21', 'a22'])
+f1 = a11**2 - sigma[0,0]
+f2 = a11*a21 - sigma[1,0]
+f3 = a21**2 + a22**2 - sigma[1,1]
+AoInvSol = nsolve((f1,f2,f3),(a11,a21,a22),(.5,-.5,.5))
+A_0_Inv = np.zeros((2,2))
+A_0_Inv[0,0] = AoInvSol[0]
+A_0_Inv[1,0] = AoInvSol[1]
+A_0_Inv[1,1] = AoInvSol[2]
+A_0 = npla.inv(A_0_Inv)
+A_0[0,1] = 0.0
 
-A_0_inv = np.array([[np.sqrt(sigma[0,0]), np.sqrt(sigma[0,1])],
-                    [0                  , np.sqrt(sigma[1,1])]])
-
+## Solving for D matricies
+# Stack betas for easy D creation
 bet = Beta[:,1:]
 betas = [np.mat(bet[:,0:2]), np.mat(bet[:,2:4]), np.mat(bet[:,4:6]),
          np.mat(bet[:,6:8]), np.mat(bet[:,8:10]), np.mat(bet[:,10:12]),
          np.mat(bet[:,12:14]), np.mat(bet[:,14:16])]
-n_vars = bet.shape[0]
 
-#big_D = []
-#D0 = np.mat(np.eye(n_vars))
-#big_D.append(D0)
-#
-#practice_D = [D0]
-#
-#for j in range(0,8):
-#    new_D =np.mat(np.zeros([2,2]))
-#    for i in range(0, 8):
-#        if j >= i:
-#            new_D += betas[j] * big_D[-i]
-#    big_D.append(new_D)
 
+## Create D matricies
 D0 = np.mat(np.eye(n_vars))
 D1 = betas[0]*D0
 D2 = betas[0]*D1 + betas[1]*D0
@@ -103,10 +94,22 @@ D7 = betas[0]*D6 + betas[1]*D5 + betas[2]*D4 + betas[3]*D3 + betas[4]*D2\
 D8 = betas[0]*D7 + betas[1]*D6 + betas[2]*D5 + betas[3]*D4 + betas[4]*D3\
                  + betas[5]*D2 + betas[6]*D1 + betas[7]*D0
 
-practice_D = [D0, D1, D2, D3, D4, D5, D6, D7, D8]
+big_D = [D0, D1, D2, D3, D4, D5, D6, D7, D8]
 
-A_0 = npla.inv(A_0_inv)
+## Get the epsilon_t by doing A_0 * resid_t
+all_eps = []
+for i in range(t):
+    temp_resid = resid[:,i]
+    eps = sp.dot(A_0, temp_resid)
+    all_eps.append(eps)
 
+## Use the epsilon_t to get C matricies. They are defined as C_i = D_i * A_0_inv
+c_ind = np.shape(big_D)[0]
+big_C = []
+for i in range(c_ind):
+    C = np.dot(big_D[i], A_0_Inv)
+    big_C.append(C)
 
+"""TODO: Fix IRFs and plot them"""
 
 
